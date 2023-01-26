75f2c7d807f393561fdb3d99cc905274091e1261
InRedwave <jisung1202@Naver.com>
2023-01-26 14:58:57 +0900

    백준 추가 문제풀이 패키지 추가

---
+++ src/other/Main_12605.java
@@ -0,0 +1,42 @@
+package other;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main_12605 {
+	
+	public String solution(String[] array) {
+		Stack<String> stack = new Stack<>();
+		for(String x : array) {
+			stack.push(x);
+		}
+		String answer = "";
+		for(String x : array) {
+			answer += stack.pop() + " ";
+		}
+		//System.out.println(answer);
+		return answer;
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		Main_12605 T = new Main_12605();
+		int N = sc.nextInt();
+		sc.nextLine();
+		String[] array = new String[N];
+		
+		for(int i= 1; i<=N; i++) {
+			array = sc.nextLine().split(" ");
+			System.out.println("Case #" + i + ": " + T.solution(array));
+//			for(String x : array) {
+//				System.out.println(x);
+//			}
+			
+		}
+		
+		
+	}
+
+}
