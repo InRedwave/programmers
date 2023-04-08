4fd2a58cda55c84aa026c4ddffc2c2e25d154f14
Jiseong Kang <112049851+InRedwave@users.noreply.github.com>
2023-04-09 01:05:20 +0900

    Update Main_1.java

--- src/inflearn/String/Main_1.java
+++ src/inflearn/String/Main_1.java
@@ -3,30 +3,35 @@ import java.util.Scanner;
 
 public class Main_1 {
 	
-	public int solution(String input1, char find){
-		int answer =0;
-	    char[] arr = input1.toLowerCase().toCharArray();
-		int count = 0;
-			
-		for(int i =0; i< arr.length; i++) {
-			if(find==arr[i]) {
-				count++;
+	public int solution(String str, char t) {
+		int answer = 0;
+		
+		str = str.toUpperCase();
+		t = Character.toUpperCase(t);
+		
+//		for(int i=0; i<str.length();i++) {
+//			if(str.charAt(i)==t) {
+//				answer ++;
+//			}
+//		}
+		
+		for(char x : str.toCharArray()) {
+			if(x==t) {
+				answer ++;
 			}
+			
 		}
-	    
-	    return count;
-	  }
-	
+
+		return answer;
+	}
 	
 	public static void main(String[] args) {
 		Main_1 main = new Main_1();
 	    Scanner sc=new Scanner(System.in);
-	    String input1 = sc.nextLine();
-		String input2 = sc.nextLine();
-	    		
-		char find = input2.toLowerCase().charAt(0);
-		System.out.print(main.solution(input1, find));
-		sc.close();
+	   String input1 = sc.next();
+		char c = sc.next().charAt(0);
+		
+		System.out.println(T.solution(input1, c));
 	}
 
 }
